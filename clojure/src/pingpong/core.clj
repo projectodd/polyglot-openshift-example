a8a14617e4d8b0a645cf7781a75791d596fb8159
Jim Crossley <jim@crossleys.org>
2012-03-27 23:20:48 -0400

    Pings and pongs

--- clojure/src/pingpong/core.clj
+++ clojure/src/pingpong/core.clj
@@ -3,20 +3,33 @@
   (:use [hiccup core page-helpers])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
-            [immutant.web :as web]))
+            [immutant.web :as web]
+            [immutant.messaging :as msg]))
 
-(defn view [msg]
+(defn layout [& msgs]
   (html5
     [:head
       [:title "Ping Pong"]
       (include-css "css/openshift.css")]
     [:body
-      [:h1 msg]]))
+      [:h1 (apply str (interpose " " msgs))]]))
+
+(defn ping []
+  (let [msg (str "hello from clojure at " (System/currentTimeMillis))]
+    (msg/publish "/queues/clojure" msg)
+    (layout "Published" msg)))
+
+(defn pong []
+  (layout "Received" (msg/receive "/queues/ruby" :timeout 100)))
 
 (defroutes main-routes
-  (GET "/" [] (view "Hello from Clojure!"))
+  (GET "/" [] (layout "Hello from Clojure!"))
+  (GET "/ping" [] (ping))
+  (GET "/pong" [] (pong))
   (route/resources "/")
   (route/not-found "Page not found"))
 
 (defn init []
+  (msg/start "/queues/ruby")
+  (msg/start "/queues/clojure")
   (web/start "/" (handler/site main-routes)))
